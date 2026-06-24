f1b5be2eb34b281ed096f03020046efcb787c151
malsha7 <malsha74bopage@gmail.com>
2026-06-24 16:39:13 +0530

    Add Harversine function

--- Program.cs
+++ Program.cs
@@ -148,7 +148,24 @@ async (string code, WasteGlassDbContext db) =>
 
 app.Run();
 
+//Harversine function to calculate distance between two lat/lon points
+static double GetDistance(double lat1, double lon1, double lat2, double lon2)
+{
+    var R = 6371;
 
+    var dLat = (lat2 - lat1) * Math.PI / 180;
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+
+    var a =
+        Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+        Math.Cos(lat1 * Math.PI / 180) *
+        Math.Cos(lat2 * Math.PI / 180) *
+        Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+    var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+    return R * c;
+}
 
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
