87fb1caf15e31dee9b387499f69193845381b3a7
malsha7 <malsha74bopage@gmail.com>
2026-06-24 17:50:40 +0530

    Did some changes

--- Program.cs
+++ Program.cs
@@ -53,24 +53,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.MapGet("/api/suppliers", async (WasteGlassDbContext db) =>
 {
@@ -264,10 +246,4 @@ static double GetDistance(double lat1, double lon1, double lat2, double lon2)
     var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
 
     return R * c;
-}
-
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
